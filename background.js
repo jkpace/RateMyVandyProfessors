876f7dc4d2dfabbde711c185490cbef541d88498
Jamal Pace <jamalkpace@gmail.com>
2015-05-24 20:56:00 -0400

    Added basic functionality for finding search page, getting results, converting name using RegEx

--- background.js
+++ background.js
@@ -6,10 +6,10 @@ chrome.runtime.onMessage.addListener(function(request, sender, callback) {
 		var method = request.method ? request.method.toUpperCase() : 'GET';
 		xhr.onload = function() {
             callback({response: xhr.responseText});
-        };
+        }
 		xhr.onerror = function() {
 			callback();
-		};
+		}
 		xhr.open(method, request.url, true);
 		if (method == 'POST') {
 			xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
--- content.js
+++ content.js
@@ -4,15 +4,16 @@
  * Jamal Pace - jamal.pace@vanderbilt.edu
  */
 
-var timeout = null;		// Necesssary for listener
+var timeout = null;		            // Necesssary for listener
+var nameFilter = /\w+(, )\w+/g;     // RegEx to change names into searchable terms
 var loc = "http://www.ratemyprofessors.com/search.jsp?queryoption=HEADER&queryBy=teacherName&schoolName=Vanderbilt+University&schoolID=4002&query=" + "gill%2C+lesley";
 
 // Confirm that the extension is active
-$("h1").append(" - Click to Run");
+$("h1").append(' - Click to run');
 
 // Placeholder click to update function until infinite loop resolved
 $("h1").click(function() {
-    searchForProfessor();
+    console.debug(convertName("Casagrande, Vivien A. | Calkins, David J."));
 })
 
 /**
@@ -28,15 +29,47 @@ function getProfessorNames() {
     }
 }
 
+/**
+ * This method changes the original name into one that can be searched
+ */
+function convertName(original) {
+    var temp = /\w+(, )\w+/g.exec(original);
+    return temp[0].replace(", ", "%2C+");
+}
+
+/**
+ * This method emulates the RMP search page then outputs 
+ * the specific ID for that professor at Vanderbilt
+ */
 function searchForProfessor() {
     chrome.runtime.sendMessage({
         action: 'xhr',
         method: 'POST',
         url: "http://www.ratemyprofessors.com/search.jsp?queryoption=HEADER&queryBy=teacherName&schoolName=Vanderbilt+University&schoolID=4002&query=gill%2C+lesley"
     }, function(response) {
+        var searchPage = document.createElement('html');
+        searchPage.innerHTML = response.response;
+        var profLink = searchPage.getElementsByClassName("listing PROFESSOR")[0].getElementsByTagName("a")[0].getAttribute("href");
+        console.debug(profLink);
+    });
+    return profLink;
+}
+
+/**
+ * This method builds on searchForProfessor by "clicking" by
+ * finding the teacher's rating from RMP and returning it.
+ */
+function findRating() {
+    chrome.runtime.sendMessage({
+        action: 'xhr',
+        method: 'POST',
+        url: "http://www.ratemyprofessors.com" + "/ShowRatings.jsp?tid=1543230"
+    }, function(response) {
         console.debug("Request received.");
-        var page = response.response;
-        console.debug(page);
+        var ratingPage = document.createElement('html');
+        ratingPage.innerHTML = response.response;
+        var profRating = ratingPage.getElementsByClassName("grade")[0].innerText;
+        console.debug("This teacher's rating is " + profRating);
     });
 }
 
@@ -57,5 +90,5 @@ function update() {
     if (timeout) {
         clearTimeout(timeout);
     }
-	timeout = setTimeout(update, 500);
+	timeout = setTimeout([METHOD TO RUN HERE], 500);
 }, false); */
