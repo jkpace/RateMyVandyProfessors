b3f5b913087818e27a22df8def26d1b31dd8f73c
Jamal Pace <jamalkpace@gmail.com>
2015-05-26 13:40:06 -0400

    Now updates automatically and has a loading indicator

--- content.js
+++ content.js
@@ -7,12 +7,26 @@ var timeout = null;		            // Necesssary for listener
 var names;                          // Set as global variable so any function can use it
 
 // Confirm that the extension is active
-$("h1").append(" - Click to run");
+$("h1").append(" - Extension active");
 
-// Placeholder click to update function until infinite loop resolved
-$("h1").click(function() {
+/**
+ * Every second, checks to see if AJAX executes (if page changes any)
+ * This allows the extension to update even though the URL never changes
+ */
+document.addEventListener("DOMSubtreeModified", function() {
+    if (timeout) {
+        clearTimeout(timeout);
+    }
+    timeout = setTimeout(update, 1000);
+}, false);
+
+/**
+ * This is a test function to test the auto-updater
+ */
+function update() {
     getProfessorNames();
-})
+    $("table.meetingPatternTable tr[1] td[4]").wrap('<a href="http://www.google.com/"></a>')       // Replaces in modal
+}
 
 /**
  * This function gets the professor name from Class Search
@@ -22,6 +36,7 @@ function getProfessorNames() {
     for (var i = 0; i < names.length; i++) {
         if (!names[i].innerText.includes(" - ")) {
             if (!names[i].innerText.includes("Staff")) {
+                names[i].innerHTML += '<img src="https://webapp.mis.vanderbilt.edu/more/images/loading.gif">'
                 searchForProfessor(i, names[i].innerText);
             } else {
                 names[i].innerText += " - N/A";
@@ -73,27 +88,19 @@ function findRating(profIndex, profName, profLink) {
         var ratingPage = document.createElement("html");
         ratingPage.innerHTML = response.pageText;
         var profRating = ratingPage.getElementsByClassName("grade")[0].innerText;
-        console.debug("Rating for " + profName + ": " + profRating);
-        names[profIndex].innerText += " - " + profRating;
+        if (parseInt(profRating) > 4.0) {
+            var color = "#27AE60";                  // Green
+        } else if (parseInt(profRating) < 3.0) {
+            var color = "#E74C3C";                  // Red
+        } else {
+            var color = "#FF9800";                  // Yellow
+        }
+        if (!names[profIndex].innerText.includes(" - ")) {
+            if (profRating != "0.0") {
+                names[profIndex].innerText += " - " + profRating;
+            } else {
+                names[profIndex].innerText += " - N/A";
+            }
+        }
     });
 }
-
-/**
- * This function replaces the professors' names with the ratings
- */
-function update() {
-    console.debug("listener fired.");
-    $(".classInstructor").wrap('<a href="http://www.google.com/"></a>');	                            // Replaces in table
-    $("table.meetingPatternTable tr:gt(0) td:gt(3)").wrap('<a href="http://www.google.com/"></a>')		// Replaces in modal
-}
-
-/**
- * Every second, checks to see if AJAX executes (if page changes any)
- * This allows the extension to update even though the URL never changes
- */
-/* document.addEventListener("DOMSubtreeModified", function() {
-    if (timeout) {
-        clearTimeout(timeout);
-    }
-	timeout = setTimeout([METHOD TO RUN HERE], 500);
-}, false); */
