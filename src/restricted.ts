4fb43e389987e7aecf8df5ce3ae654235fee902a
Jamal Pace <jamalkpace@gmail.com>
2017-12-18 15:39:41 -0500

    Ignore restricted.ts

--- src/restricted.ts
+++
@@ -1,8 +0,0 @@
-// Names that should not be rated
-// Be sure to keep in alphabetical order
-// Be sure to list the reason for each professor above the name
-
-export const restricted: string[] = [
-  // There are two "William Robinson"s at Vanderbilt and this one is not on RateMyProfessors
-  'Robinson, William F.',
-];
